69a251b19843b722e3621729a2a49ef352b64ab4
will4769 <wisley@esri.com>
2016-09-29 18:47:14 -0700

    Added more schema elements

--- src/main/java/com/cgu/ist303/project/dao/model/Payment.java
+++ src/main/java/com/cgu/ist303/project/dao/model/Payment.java
@@ -4,4 +4,17 @@ package com.cgu.ist303.project.dao.model;
  * Created by will4769 on 9/29/16.
  */
 public class Payment {
+    public double getAmount() {
+        return amount;
+    }
+
+    //TODO: impliment this
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    private double amount = 0.0;
+
+    public Payment() {
+    }
 }
