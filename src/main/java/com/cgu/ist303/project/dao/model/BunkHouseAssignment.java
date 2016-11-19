98f00b41700efe624a3aa665c3ec43c048575bfb
will4769 <wisley@esri.com>
2016-11-19 12:44:37 -0800

    Created BunkHouseAssignmentModel

--- src/main/java/com/cgu/ist303/project/dao/model/BunkHouseAssignment.java
+++ src/main/java/com/cgu/ist303/project/dao/model/BunkHouseAssignment.java
@@ -4,8 +4,23 @@ package com.cgu.ist303.project.dao.model;
  * Created by will4769 on 9/29/16.
  */
 public class BunkHouseAssignment {
+    public Camper getCamper() {
+        return camper;
+    }
 
-    //a camper and a bunkhouse
+    public void setCamper(Camper camper) {
+        this.camper = camper;
+    }
 
+    private Camper camper = null;
 
+    public BunkHouse getBunkHouse() {
+        return bunkHouse;
+    }
+
+    public void setBunkHouse(BunkHouse bunkHouse) {
+        this.bunkHouse = bunkHouse;
+    }
+
+    private BunkHouse bunkHouse = null;
 }
