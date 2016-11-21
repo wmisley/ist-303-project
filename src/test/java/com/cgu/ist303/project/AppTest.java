354337b447c3d9625cfb3a80d85e5690db016192
will4769 <wisley@esri.com>
2016-11-20 20:46:21 -0800

    Add JUnit support

---
+++ src/test/java/com/cgu/ist303/project/AppTest.java
@@ -0,0 +1,15 @@
+package com.cgu.ist303.project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by will4769 on 11/20/16.
+ */
+public class AppTest {
+    @Test
+    public void testApp() throws Exception {
+        assertEquals("", "");
+    }
+}
