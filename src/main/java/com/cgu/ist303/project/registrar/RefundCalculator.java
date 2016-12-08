60a13b3d0a27184d7192fa4c345844100e49441c
will4769 <wisley@esri.com>
2016-12-07 21:25:53 -0800

    Added support to cancel registration. Still need to propertly calculate refund.

--- src/main/java/com/cgu/ist303/project/registrar/RefundCalculator.java
+++ src/main/java/com/cgu/ist303/project/registrar/RefundCalculator.java
@@ -1,33 +1,34 @@
 package com.cgu.ist303.project.registrar;
 
-import com.cgu.ist303.project.dao.model.CampSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class RefundCalculator {
+    private static final Logger log = LogManager.getLogger(RefundCalculator.class);
+
+
     public RefundCalculator() {
     }
 
     public long calculateNumberOfDays(Date dt1, Date dt2) {
         long timeDiff = dt1.getTime() - dt2.getTime();
         long diffDays = timeDiff / (60 * 60 * 24 * 1000);
-        return diffDays;
+        return Math.abs(diffDays);
     }
 
-    public double calculteRefund(double paidAmount, Date datePacketMailed) {
+    public double calculteRefund(double paidAmount, Date dateToday, Date datePacketMailed) {
         final long DAYS_IN_THREE_WEEKS = 21;
         final long DAYS_IN_SIX_WEEKS = 42;
 
-        long days = calculateNumberOfDays(new Date(), datePacketMailed);
+        long days = calculateNumberOfDays(dateToday, datePacketMailed);
         double perCentageRefund = 1.0;
         double refund = paidAmount;
 
         if (days <= DAYS_IN_THREE_WEEKS) {
             perCentageRefund = 0.9;
-        } else if (days < DAYS_IN_SIX_WEEKS) {
+        } else if (days <= DAYS_IN_SIX_WEEKS) {
             perCentageRefund = 0.65;
         } else {
             perCentageRefund = 0.0;
---
+++ src/test/java/com/cgu/ist303/project/registrar/RefundCalculatorTest.java
@@ -0,0 +1,121 @@
+package com.cgu.ist303.project.registrar;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class RefundCalculatorTest {
+    public RefundCalculatorTest() {
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void calculateNumberOfDaysStartBeforeEndTest() {
+        RefundCalculator rc = new RefundCalculator();
+
+        Calendar cal1 = new GregorianCalendar(2016, 11, 31);
+        Date dat1 = cal1.getTime();
+
+        Calendar cal2 = new GregorianCalendar(2017, 0, 1);
+        Date dat2 = cal2.getTime();
+
+        Assert.assertEquals(rc.calculateNumberOfDays(dat1, dat2), 1);
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void calculateNumberOfDaysStartAfterEndTest() {
+        RefundCalculator rc = new RefundCalculator();
+
+        Calendar cal1 = new GregorianCalendar(2016, 11, 31);
+        Date dat1 = cal1.getTime();
+
+        Calendar cal2 = new GregorianCalendar(2017, 0, 1);
+        Date dat2 = cal2.getTime();
+
+        Assert.assertEquals(rc.calculateNumberOfDays(dat2, dat1), 1);
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void calculateRefund90PercentFirstDay() {
+        RefundCalculator rc = new RefundCalculator();
+
+        Calendar cal1 = new GregorianCalendar(2016, 11, 31);
+        Date dat1 = cal1.getTime();
+
+        Calendar cal2 = new GregorianCalendar(2017, 0, 1);
+        Date dat2 = cal2.getTime();
+
+        double refund = rc.calculteRefund(100.0, dat1, dat2);
+
+        Assert.assertTrue((refund - 90.0) < .001);
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void calculateRefund90PercentLastDay() {
+        RefundCalculator rc = new RefundCalculator();
+
+        Calendar cal1 = new GregorianCalendar(2016, 11, 31);
+        Date dat1 = cal1.getTime();
+
+        Calendar cal2 = new GregorianCalendar(2017, 0, 21);
+        Date dat2 = cal2.getTime();
+
+        double refund = rc.calculteRefund(100.0, dat1, dat2);
+
+        Assert.assertTrue((refund - 90.0) < .001);
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void calculateRefund65PercentFirstDay() {
+        RefundCalculator rc = new RefundCalculator();
+
+        Calendar cal1 = new GregorianCalendar(2016, 11, 31);
+        Date dat1 = cal1.getTime();
+
+        Calendar cal2 = new GregorianCalendar(2017, 0, 22);
+        Date dat2 = cal2.getTime();
+
+        double refund = rc.calculteRefund(100.0, dat1, dat2);
+
+        Assert.assertTrue((refund - 65.0) < .001);
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void calculateRefund65PercentLastDay() {
+        RefundCalculator rc = new RefundCalculator();
+
+        Calendar cal1 = new GregorianCalendar(2016, 11, 31);
+        Date dat1 = cal1.getTime();
+
+        Calendar cal2 = new GregorianCalendar(2017, 1, 11);
+        Date dat2 = cal2.getTime();
+
+        double refund = rc.calculteRefund(100.0, dat1, dat2);
+
+        Assert.assertTrue((refund - 65.0) < .001);
+    }
+
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void calculateRefund0PercentFirstDay() {
+        RefundCalculator rc = new RefundCalculator();
+
+        Calendar cal1 = new GregorianCalendar(2016, 11, 31);
+        Date dat1 = cal1.getTime();
+
+        Calendar cal2 = new GregorianCalendar(2017, 1, 12);
+        Date dat2 = cal2.getTime();
+
+        double refund = rc.calculteRefund(100.0, dat1, dat2);
+        Assert.assertTrue(refund < .001);
+    }
+}
